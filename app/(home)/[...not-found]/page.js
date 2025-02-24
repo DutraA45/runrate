197471c44dbae6d8600bf8682171b9ffe442f063
Jefferson <jeffinho.angelo@gmail.com>
2025-02-24 15:58:09 -0300

    color-text-not-found

--- app/(home)/[...not-found]/page.js
+++ app/(home)/[...not-found]/page.js
@@ -1,6 +1,6 @@
 export default function HomeNotFound() {
     return (
-      <div style={{ textAlign: 'center', padding: '50px' }}>
+      <div style={{ textAlign: 'center', padding: '50px', color: "#000"}}>
         <h1>Erro 404</h1>
         <p>A página da Home não foi encontrada.</p>
         <a href="/">Voltar para a Home</a>
